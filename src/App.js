bb32d11f23d371c7503f13a9501c3029a1ab6da2
Edgar Haro <harove@gmail.com>
2021-05-08 19:57:34 -0400

    useReducer hook implemented

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import "./styles.css";
 import { useReducer } from "react";
 
 export default function App() {
-  const nums = [2, 4, 6];
-  const initialState = 0;
+  const [counter, dispatch] = useReducer(reducer, 0);
 
-  function reducer(state, value, currentIndex, arr) {
-    console.log(currentIndex);
-    console.log(arr);
+  function reducer(state, value) {
     return state + value;
   }
 
-  let sum = nums.reduce(reducer, initialState);
-
-  console.log(sum);
-  console.log(reducer(2, 1));
-
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input type="button" onClick={(e) => dispatch(2)} value={counter} />
     </div>
   );
 }
